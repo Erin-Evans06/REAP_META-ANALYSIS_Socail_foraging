81c3c899dd80baf35a9eda446eb0ed20dac0b753
“Erin-Evans06” <kevans@oxy.edu>
2024-07-10 10:29:10 -0700

    moved location of legened

--- prevalence_by_year_figure.R
+++ prevalence_by_year_figure.R
@@ -9,12 +9,18 @@ head(year_data)
 year_sum <- year_data %>%
   group_by(Year) %>%
   add_count(Data)
+# Define colors for 'Yes' and 'No'
+data_colors <- c("Yes" = "cyan3", "No" = "coral")
 
-#Plot Data into
-ggplot(year_sum, aes(x=Year,y=n,color=Data)) +
-  geom_line() + labs(title = "Prevalence of Research over Time",
-                     x = "Year",
-                     y = "Number of Publications") +
-  theme_classic()+theme(plot.title = element_text(hjust=0.5))
+# Plot Data
+ggplot(year_sum, aes(x=Year, y=n, color=Data)) +
+  geom_line() +
+  labs(title = "Prevalence of Research over Time",
+       x = "Year",
+       y = "Number of Publications") +
+  theme_classic() +
+  theme(plot.title = element_text(hjust=0.5),,legend.position = c(0.15, 0.8)) +
+  scale_color_manual(values = data_colors)
+
 
 
