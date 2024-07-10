2fff544c1879f7bc08ba6a87d459f3fb7c143f25
“Erin-Evans06” <kevans@oxy.edu>
2024-07-10 11:32:02 -0700

    added csv for consumption rate vs group size changed legend

--- n_vs_Date_figure.R
+++ n_vs_Date_figure.R
@@ -3,7 +3,7 @@ library(ggplot2)
 library(tidyverse)
 library(lubridate)
 #Load .csv and name fig_b
-fig_b <- read.csv("n_vs_date.csv")
+fig_b <- read.csv("n_vs_dateold.csv")
 #Look at data
 head(fig_b)
 #format date for R
@@ -12,7 +12,7 @@ fig_b$Publication.Date <- ym(fig_b$Publication.Date)
 str(fig_b)
 #Plot data
 ggplot(fig_b, aes(x=Publication.Date,y=Sample.Size)) + 
-  geom_point(color="blue",pch=21,alpha=0.6,size=3) + 
+  geom_point(color="cyan3",pch=19,alpha= 0.5, size=3) + 
   labs(title = "Sample Size vs Publication Date",x = "Date",y = "Sample Size") + 
   theme_classic()+theme(plot.title = element_text(hjust=0.5))+
   scale_y_continuous(expand = expansion(mult = c(0, 0.1))) +
