a4bd9a2fcbbe6307b7db5b11d73b3fcd95a726df
“Erin-Evans06” <kevans@oxy.edu>
2024-07-22 15:20:52 -0700

    Made elements bigger

--- prevalence_by_year_figure.R
+++ prevalence_by_year_figure.R
@@ -13,14 +13,21 @@ year_sum <- year_data %>%
 data_colors <- c("Yes" = "cyan3", "No" = "coral")
 
 # Plot Data
-ggplot(year_sum, aes(x=Year, y=n, color=Data)) +
-  geom_line() +
-  labs(title = "Prevalence of Research over Time",
-       x = "Year",
-       y = "Number of Publications") +
+ggplot(year_sum, aes(x=Year, y=n, fill=Data)) +
+  geom_col() +
+  labs(x = NULL,y = NULL)+
   theme_classic() +
-  theme(plot.title = element_text(hjust=0.5),,legend.position = c(0.15, 0.8)) +
+  theme(plot.title = element_text(hjust=0.5),
+        legend.position = c(0.15, 0.8),
+        legend.key.size = unit(1, "cm"),
+        legend.text = element_text(size=12),
+        axis.line = element_line(size=0.9),
+        axis.ticks = element_line(size=1),
+        axis.line.x = element_line(color = "black", size = 1),
+        axis.line.y = element_line(color = "black", size = 1),
+        axis.text = element_text(size = 14, face = "bold")) +
   scale_color_manual(values = data_colors)
+  
 
 
 
